281319ab3281730e483fb7b43a6e47e00ab7f4dc
Renaldi Iskandar <71915925+Restkand@users.noreply.github.com>
2022-10-11 20:14:19 +0000

    Add more function with Pilihan Barang and Count with Amortisasi

--- cashFunction.py
+++ cashFunction.py
@@ -1,10 +1,40 @@
 class cashFunc:
   # Fungsi Menghitung Cash
       def __init__ (self) :
+        self.barang = {
+          "housing" : 20,
+          "transportation" : 10,
+          "gadget" : 5,
+          "travel" : 0.08,
+        }
+
+      def inputSys (self):
         self.penghasilan = int(input('Masukan Pemasukan Bulanan : '))
         self.pengeluaran = int(input('Harga Barang yang ingin di beli : '))
+        self.pilihBarang = input("\nBarang yang ingin di beli : \n1.Housing\n2.Transportation\n3.Gadget\n4.Travel\nPilih Barang : ").lower()
+        
+      def countCash(self):
         self.wants = float(self.penghasilan) * 0.30
-        self.hasil = int(self.wants - self.pengeluaran)
+        self.hasil = float(self.wants - self.pengeluaran)
         self.sisa = float(self.hasil) / self.penghasilan * 100
-        print(str(self.hasil))
-        print(str("{:.2f}".format(self.sisa)) + "%")
+
+      def count(self):
+        self.countCash()
+        if self.sisa <= 0:
+          print("\nKarena udah melebihi perkiraan Assets kamu, maka kami menghitung dengan Amortisasi")
+          if self.pilihBarang in ['housing','1' ,'rumah']:
+            self.pengeluaran = self.pengeluaran / self.barang['housing']/12
+            self.countCash()
+          elif self.pilihBarang in ['transportation', '2','transportasi']:
+            self.pengeluaran = self.pengeluaran /self.barang['transportation']/12
+            self.countCash()
+          elif self.pilihBarang in ['gadget', '3','smartphone']:
+            self.pengeluaran = self.pengeluaran /self.barang['gadget']/12
+            self.countCash()
+          elif self.pilihBarang in ['travel', '4','tiket']:
+            self.pengeluaran = self.pengeluaran /self.barang['travel']/12
+            self.countCash()
+            
+        print(str("\nSisa uang kamu : {:.2f}".format(self.hasil)))
+        print(str("Dan sisa Wants kamu {:.2f}".format(self.sisa)) + "%")   
+            
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from cashFunction import cashFunc
 
 print('Wants App')
 cashFunc = cashFunc()
-if(cashFunc.sisa >= 20):
+cashFunc.inputSys()
+cashFunc.count()
+if (cashFunc.sisa >= 20):
   print("Boleh banget nih buat menuhin wants kamu sesekali")
-elif(cashFunc.sisa >= 10):
+elif (cashFunc.sisa >= 10):
   print("Coba kamu pertimbangin lagi")
-elif(cashFunc.sisa >= 1):
+elif (cashFunc.sisa >= 1):
   print("Waduh mending ditahan dulu beli barangnya")
 else:
   print("Big No! Udah melebihi budget kamu ni!")
